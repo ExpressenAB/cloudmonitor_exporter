89625eae9782dad2c024ece980e010f039a19d86
Rickard Karlsson <rickard.karlsson@bonniernews.se>
2016-09-22 12:57:39 +0200

    Change to generate cert

--- setup/setup.sh
+++ setup/setup.sh
@@ -3,6 +3,6 @@
 if [ ! -f cert.pem ]; then
 	echo "This will generate a self-signed certificate to use with cloudmonitor_exporter\nEnter companyname for certificate: "
 	read company
-	openssl req -x509 -newkey rsa:2048 -keyout key.pem -out ca.pem -days 1080 -nodes -subj '/CN=*/O=$input./C=US' && cp key.pem cert.pem && cat ca.pem >> cert.pem
+	openssl req -x509 -newkey rsa:2048 -keyout key.pem -out ca.pem -days 1080 -nodes -subj "/CN=*/O=$company./C=US" && cp key.pem cert.pem && cat ca.pem >> cert.pem
 fi
 
